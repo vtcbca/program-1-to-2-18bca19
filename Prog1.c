ca2b0734df8ba8519febbe5d4bc8bc4dbcf1acd7
18bca19 <77221770+18bca19@users.noreply.github.com>
2021-11-10 18:49:58 +0530

    Uploading solution of program 1.
    Show incorrect answer...Please tell me where I made a mistake

---
+++ Prog1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int x,y,z;
+    printf("\nEnter valuse for a:");
+    scanf("%d",&x);
+    printf("\nEnter value for b:");
+    scanf("%d",y);
+    z=x+y;
+    printf("\n\nSum is %d",z);
+    return 0;
+}
+
